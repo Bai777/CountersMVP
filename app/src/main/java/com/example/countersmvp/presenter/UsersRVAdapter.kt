35ebd45641ef4b8668c29d7e6b3956b5bc1d8511
Stepan <bai_777@mail.ru>
2021-11-02 18:29:39 +0300

    создал класс UserRVAdapter

---
+++ app/src/main/java/com/example/countersmvp/presenter/UsersRVAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.countersmvp.presenter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.countersmvp.databinding.ItemUserBinding
+import com.example.countersmvp.view.UserItemView
+
+class UsersRVAdapter(val presenter: IUserListPresenter) : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)).apply {
+            itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
+        }
+
+    override fun getItemCount() = presenter.getCount()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = presenter.bindView(holder.apply { pos = position })
+
+    inner class ViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root), UserItemView {
+        override var pos = -1
+
+        override fun setLogin(text: String) = with(binding) {
+            tvLogin.text = text
+        }
+    }
+}
