58ab8d6e1099a3f73e5b4311b966cd79e044131d
Stepan <bai_777@mail.ru>
2021-12-12 17:12:35 +0300

    изменил GitHubUserRepository.kt и GitHubUser.kt

--- app/src/main/java/com/example/countersmvp/model/GitHubUser.kt
+++ app/src/main/java/com/example/countersmvp/model/GitHubUser.kt
@@ -1,11 +1,12 @@
 package com.example.countersmvp.model
 
 import android.os.Parcelable
+import com.example.countersmvp.R
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class GitHubUser(
-    val login: String,
-    val password: String
+    private val login: String = R.id.et_login.toString(),
+    private val password: String = R.id.et_password.toString()
 ): Parcelable
 
--- app/src/main/java/com/example/countersmvp/model/GitHubUserRepository.kt
+++ app/src/main/java/com/example/countersmvp/model/GitHubUserRepository.kt
@@ -1,18 +1,7 @@
 package com.example.countersmvp.model
 
-import com.example.countersmvp.R
-import java.util.ArrayList
+import io.reactivex.rxjava3.core.Observable
 
-class GitHubUserRepository {
-    private val login = R.id.et_login
-    private val password = R.id.et_password
-
-    fun set():ArrayList<String> {
-        val list = ArrayList<String>()
-        val userLogin = login.toString()
-        val userPassword = password.toString()
-        list.add(userLogin)
-        list.add(userPassword)
-        return list
-    }
+interface GitHubUserRepository {
+    fun getUsers():Observable<List<GitHubUser>>
 }
